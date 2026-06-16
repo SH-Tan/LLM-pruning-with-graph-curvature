2b4225011605030937b518e0d4f6173d20220c37
sh <tshddo@outlook.com>
2026-06-16 16:05:02 -0400

    delete residual

--- graph_relation.py
+++ graph_relation.py
@@ -3,8 +3,8 @@ from curv_distribution_utils import _resolve_node_name
 GRAPH = {
     "prev_down_proj": {"layer": 0, "prev": ["prev_up_proj", "prev_gate_proj"], "next": ["v_proj"], "prev_in": ["prev_o_proj"], "next_out": ["v_proj"]},
     "q_proj": {"layer": 1, "prev": ["prev_down_proj"], "next": ["k_proj"], "prev_in": ["prev_gate_up_out"], "next_out": ["A"]},
-    "k_proj": {"layer": 1, "prev": ["prev_down_proj"], "next": ["q_proj"], "prev_in": ["prev_gate_up_out"], "next_out": ["A"], "residual": ["prev_qkv_residual"]},
-    "v_proj": {"layer": 1, "prev": ["prev_down_proj"], "next": ["A"], "prev_in": ["prev_gate_up_out"], "next_out": ["Att_out"], "residual": ["prev_qkv_residual"]},
+    "k_proj": {"layer": 1, "prev": ["prev_down_proj"], "next": ["q_proj"], "prev_in": ["prev_gate_up_out"], "next_out": ["A"]},
+    "v_proj": {"layer": 1, "prev": ["prev_down_proj"], "next": ["A"], "prev_in": ["prev_gate_up_out"], "next_out": ["Att_out"]},
     "A": {"layer": 2, "prev": [], "next": ["v_proj"], "prev_in": [], "next_out": ["v_proj"]},
     "o_proj": {"layer": 3, "prev": ["A"], "next": ["gate_proj", "up_proj"], "prev_in": ["v_proj"], "next_out": ["gate_up_out"]},
     "gate_proj": {"layer": 4.1, "prev": ["o_proj"], "next": ["down_proj"], "prev_in": ["Att_out"], "next_out": ["down_proj"]},
@@ -12,10 +12,6 @@ GRAPH = {
     "down_proj": {"layer": 5, "prev": ["up_proj", "gate_proj"], "next": ["v_proj"], "prev_in": ["o_proj"], "next_out": ["lm_head"]},
     "lm_head": {"layer": 6, "prev": ["down_proj"], "next": [], "prev_in": ["gate_up_out"], "next_out": []},
 }
-
-# "residual": ["prev_qkv_residual"]
-
-
 
 def _resolve_graph_sets(operations, short_name):
     rel = GRAPH.get(short_name, {})
